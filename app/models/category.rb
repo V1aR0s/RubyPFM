5eeb152601f658429a1e7fec8754c599cbf34405
Roschupkin Vlad <vlaros19992004@gmail.com>
2023-02-05 14:03:15 +0200

    created validation for category and operation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
   has_many :operations
+  validates :name, uniqueness:true, presence:true
+  validates :description, presence:true
 end
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,5 @@
 class Operation < ApplicationRecord
   belongs_to :category
+  validates :odate, presence:true
+  validates :amount, presence:true, numericality: { greater_than: 0}
 end
