fa9ba2488f09389daa22b412b92674fa97a323d4
Roschupkin Vlad <vlaros19992004@gmail.com>
2023-02-05 15:39:08 +0200

    added Reports Views and Controller

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,14 @@
+class ReportsController < ApplicationController
+  def index
+  end
+
+  def report_by_category
+    @operations = Operation.all
+    @categories = Category.all
+  end
+
+  def report_by_dates
+    @operations = Operation.all
+    @categories = Category.all
+  end
+end
