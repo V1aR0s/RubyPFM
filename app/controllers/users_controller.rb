f484ab2c41b8d0ce2d77f89595b113ca0ed9959f
Vlad Roschupkin <vlaros19992004@gmail.com>
2023-02-06 13:44:09 +0200

    created user model, registration page, linked bootstrap

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:name, :surname, :email)
+
+    User.create(user_params)
+
+
+    redirect_to root_path, notice: "Вы успешона зарегестрировались!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'main#index'
+
   get 'reports/index'
   get 'reports/report_by_category'
   get 'reports/report_by_dates'
   get 'main/index'
-  root 'main#index'
+ 
+
+  resources :users, only: %i[new create]
   resources :operations
   resources :categories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
