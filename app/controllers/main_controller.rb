d5da46f84e26cb7a4681d256f54d2c36a2dd9939
Roschupkin Vlad <vlaros19992004@gmail.com>
2023-02-09 15:39:06 +0200

    added donut graphic on main page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -18,8 +18,8 @@ class MainController < ApplicationController
     @current_month_first_day = Date.today.beginning_of_month
     @current_month_last_day = Date.today.end_of_month
 
-    @last_op = @user.operations.limit(5).order("odate")
-      .where(:odate => @current_month_first_day..@current_month_last_day).reverse
+    @last_op = @user.operations.order("odate")
+      .where(:odate => @current_month_first_day..@current_month_last_day).last(3).reverse
 
 
 
