dfef3d673a165b3226df4b985f212167be8be590
Vlad Roschupkin <vlaros19992004@gmail.com>
2023-02-19 20:38:34 +0200

    added redirect to report_by_category/report_by_dates mechanizme  from report index

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,13 +1,19 @@
 class ReportsController < ApplicationController
   def index
 
-
+    if params[:clear].present?
+      render 'index'
+      puts("""""""""""")
+    end
     
+    @category_id = params[:category_id]
+    redirect_to reports_report_by_category_path(category_id: params[:category_id])
   end
 
   def report_by_category
-    @operations = Operation.all
-    @categories = Category.all
+    #@operations = Operation.all
+    #@categories = Category.all
+    @cat_id = params[:category_id]
   end
 
   def report_by_dates
