7221f2f446d121f9ec2c634651cda4953825c2b2
Roschupkin Vlad <vlaros19992004@gmail.com>
2023-02-20 17:26:42 +0200

    added mechanizm check type of graphic on report page index

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,11 +3,35 @@ class ReportsController < ApplicationController
 
     if params[:clear].present?
       render 'index'
-      puts("""""""""""")
     end
-    
-    @category_id = params[:category_id]
-    redirect_to reports_report_by_category_path(category_id: params[:category_id])
+
+
+    if params[:generate].present?
+
+
+      @first_date = params[:first_date]# Date.today.beginning_of_month
+      @last_date = params[:last_date]
+      if params[:first_date] == nil || params[:first_date]
+          @first_date = Date.today.beginning_of_month
+          @last_date = Date.today.end_of_month
+      end
+
+
+      @category_id = params[:category_id]
+
+
+
+
+      if params[:type_graph].to_i == 1
+        redirect_to reports_report_by_dates_path(category_id: params[:category_id], first_date: @first_date, last_date:@last_date, income:params[:income])
+      else
+        redirect_to reports_report_by_category_path(category_id: params[:category_id], first_date: @first_date, last_date:@last_date, income:params[:income])
+      end
+
+
+
+    end
+
   end
 
   def report_by_category
