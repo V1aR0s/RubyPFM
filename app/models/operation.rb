1661e155123a104a21b2d8883a97dd953b109568
Vlad Roschupkin <vlaros19992004@gmail.com>
2023-03-01 17:46:58 +0200

    psuhed on hosting

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,7 +2,7 @@ class Operation < ApplicationRecord
   belongs_to :category
   belongs_to :user
   validates :description, presence:true
-  validates :odate, presence:true, inclusion: { in: ->(date) { Date.new..DateTime.now } }
+  validates :odate, presence:true, inclusion: { in: ->(date) { DateTime.new..DateTime.now } }
   validates :amount, presence:true, numericality: {greater_than: 0}
 
 
